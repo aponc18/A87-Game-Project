c8ce4fd848a6f8c2125cdc4a671eb10a5ee63b53
aponc18 <alexispon18@outlook.com>
2022-09-30 13:02:28 -0700

    c2

---
+++ A87 Game/Assets/Scripts/Gun.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField] GunData gunData;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerShoot.shootInput += Shoot;
+    }
+
+    public void Shoot()
+    {
+        Debug.Log("Shot Gun");
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ A87 Game/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public static Action shootInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shootInput.GetMouseButton(0))
+        {
+            shootInput?.Invoke();
+        }
+    }
+}
