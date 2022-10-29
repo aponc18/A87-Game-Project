5bd047aca40caf258edc57e8b208cb4963f7d763
aponc18 <alexispon18@outlook.com>
2022-10-28 18:01:43 -0700

    Weapon swap

--- A87 Game/Assets/Scripts/Weapon Scripts/WeaponHandler.cs
+++ A87 Game/Assets/Scripts/Weapon Scripts/WeaponHandler.cs
@@ -4,18 +4,40 @@ using UnityEngine;
 
 public class WeaponHandler : MonoBehaviour
 {
-    [SerializeField] private GunWeapon currentGun;
+    [SerializeField] private List<GunWeapon> guns = new List<GunWeapon>();
 
+    private GunWeapon currentGun;
     private Transform cameraTransform;
+    private GameObject currentGunPrefab;
 
     private void Start() 
     {
         cameraTransform = Camera.main.transform;
+        currentGunPrefab = Instantiate(guns[0].gunPrefab, transform);
+        currentGun = guns[0];
     }
 
     private void Update() 
     {
         CheckForShooting();
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Destroy(currentGunPrefab);
+            currentGunPrefab = Instantiate(guns[0].gunPrefab, transform);
+            currentGun = guns[0];
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Destroy(currentGunPrefab);
+            currentGunPrefab = Instantiate(guns[1].gunPrefab, transform);
+            currentGun = guns[1];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            Destroy(currentGunPrefab);
+            currentGunPrefab = Instantiate(guns[2].gunPrefab, transform);
+            currentGun = guns[2];
+        }
     }
 
     private void CheckForShooting()
