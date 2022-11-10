ff1dc8fd8d854625bbf88a92998dc1a624b20018
aponc18 <alexispon18@outlook.com>
2022-11-10 14:48:08 -0800

    Exit door trigger

---
+++ A87 Game/Assets/Scripts/TriggerDoorController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDoorController : MonoBehaviour
+{
+    [SerializeField] private Animator myDoor = null;
+    //[SerializeField] private bool openTrigger = false;
+
+    private GameObject[] targets;
+    public void Update() 
+    {
+        OntriggerEnter();
+        
+    }
+
+    private void OntriggerEnter() 
+    {
+        targets = GameObject.FindGameObjectsWithTag("Objective");
+        Debug.LogError(""+targets.Length);
+
+        if (GameObject.FindGameObjectWithTag("Objective") == null)
+        {
+            //opendoor
+            myDoor.Play("DoorRight", 0, 0.0f);
+            myDoor.Play("DoorLeft", 0, 0.0f);
+            
+        }
+        else 
+        {
+            //do nothing
+            Debug.LogError("There are still Targets");
+        }
+    }
+}
