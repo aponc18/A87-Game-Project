0de803909ef06462221a8eddb89a666f06f1600f
RAtianz <ratianz@calstatela.edu>
2022-11-15 00:22:51 -0800

    Main Menu Added

---
+++ A87 Game/Assets/MainMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    
+    public void playGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void continueGame ()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void quitGame ()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+}
