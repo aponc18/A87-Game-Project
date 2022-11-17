804b8a34a4f3a5df8c6dd1d15e2fff5699bb18aa
aponc18 <alexispon18@outlook.com>
2022-11-16 17:10:27 -0800

    healthbars

--- A87 Game/Assets/Scripts/EnemyScipts/EnemyHealth.cs
+++ A87 Game/Assets/Scripts/EnemyScipts/EnemyHealth.cs
@@ -1,23 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealth : MonoBehaviour, IDamagable
 {
     [SerializeField] private EnemyStats enemyStats;
+    [SerializeField] private Slider healthBar;
+    [SerializeField] private Image healthBarImage;
+    [SerializeField] private Color maxHealthColor;
+    [SerializeField] private Color minHealthColor;
 
     private int currentHealth;
 
     private void Start() 
     {
         currentHealth = enemyStats.maxHealth;
+        SetHealthbarUI();
     }
 
     public void Damage(int damage) 
     {
         currentHealth -= damage;
         CheckIfDead();
-        
+        SetHealthbarUI();
     }
 
     private void CheckIfDead() 
@@ -28,4 +34,15 @@ public class EnemyHealth : MonoBehaviour, IDamagable
         }
     }
 
+    private void SetHealthbarUI() 
+    {
+        float healthPer = CalculateHealthPerc();
+        healthBar.value = healthPer;
+        healthBarImage.color = Color.Lerp(minHealthColor, maxHealthColor, healthPer / 100);
+    }
+
+    private float CalculateHealthPerc() 
+    {
+        return ((float)currentHealth / (float)enemyStats.maxHealth) * 100;
+    }
 }
