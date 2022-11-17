4df20cd2295a2f88a038b6bda9b07339e49502df
aponc18 <alexispon18@outlook.com>
2022-11-17 15:28:07 -0800

    update

--- A87 Game/Assets/Scripts/EnemyScipts/EnemyHealth.cs
+++ A87 Game/Assets/Scripts/EnemyScipts/EnemyHealth.cs
@@ -10,6 +10,7 @@ public class EnemyHealth : MonoBehaviour, IDamagable
     [SerializeField] private Image healthBarImage;
     [SerializeField] private Color maxHealthColor;
     [SerializeField] private Color minHealthColor;
+    [SerializeField] private bool regenerator;
 
     private int currentHealth;
 
@@ -17,6 +18,12 @@ public class EnemyHealth : MonoBehaviour, IDamagable
     {
         currentHealth = enemyStats.maxHealth;
         SetHealthbarUI();
+        
+    }
+    private void Update() 
+    {
+        //RegainHealth();
+        //SetHealthbarUI();
     }
 
     public void Damage(int damage) 
@@ -24,6 +31,29 @@ public class EnemyHealth : MonoBehaviour, IDamagable
         currentHealth -= damage;
         CheckIfDead();
         SetHealthbarUI();
+    }
+
+   /** public void Regen() 
+    {
+        if (regenerator)
+        {
+            while (currentHealth < enemyStats.maxHealth && currentHealth > 0)
+            {
+                currentHealth++;
+            }
+        }
+    }**/
+
+    IEnumerator RegainHealth() 
+    {
+        if (regenerator) 
+        {
+            while (currentHealth < enemyStats.maxHealth && currentHealth > 0)
+            {
+                yield return new WaitForSeconds(1f);
+                currentHealth++;
+            }
+        }
     }
 
     private void CheckIfDead() 
